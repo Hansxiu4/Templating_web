a5e7e95096edcf027be7a2c19c2858cdaf6f4469
Farhan <hansxiu4@gmail.com>
2024-11-05 13:27:25 +0700

    Add files via upload

---
+++ Templat_PHP/sidebar.php
@@ -0,0 +1,8 @@
+<aside class="sidebar">
+  <h2>Sidebar Title</h2>
+  <ul>
+    <li><a href="link1.php">Link 1</a></li>
+    <li><a href="link2.php">Link 2</a></li>
+    <li><a href="link3.php">Link 3</a></li>
+  </ul>
+</aside>
